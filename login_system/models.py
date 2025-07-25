66a7962957860b9e70ee4e6b14bbd152b3e5c3fe
Yhefritd <huachomontes123.1@gmail.com>
2025-07-24 20:35:08 -0500

    Pagina v1.3 Mejorando las funcionalidades del Registro

--- login_system/models.py
+++ login_system/models.py
@@ -4,8 +4,8 @@ class Usuario(models.Model):
     id = models.AutoField(primary_key=True)  # ID auto_increment
     nombre = models.CharField(max_length=100)
     correo = models.EmailField(max_length=100, unique=True)
-    telefono = models.CharField(max_length=15, blank=True, null=True)
     contraseña = models.CharField(max_length=100)  # Para la contraseña encriptada
+    telefono = models.CharField(max_length=15, blank=True, null=True)
 
     def __str__(self):
         return self.nombre
--- login_system/views.py
+++ login_system/views.py
@@ -1,8 +1,33 @@
 
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Usuario
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def registro(request):
+    if request.method == 'POST':
+        # Obtén los datos del formulario
+        nombre = request.POST['nombre']
+        correo = request.POST['correo']
+        contraseña = request.POST['contraseña']
+        telefono = request.POST.get('telefono', '')
+
+        print(f"Nombre: {nombre}, Correo: {correo}, Contraseña: {contraseña}, Teléfono: {telefono}")  # Para depuración
+
+        # Crea un nuevo usuario y guárdalo en la base de datos
+        usuario = Usuario(
+            nombre=nombre,
+            correo=correo,
+            contraseña=contraseña,  # Recuerda que deberías encriptar la contraseña antes de guardarla
+            telefono=telefono
+        )
+        usuario.save()
+
+        # Redirige a una página de éxito o de inicio de sesión
+        return redirect('login')  # O la URL a la que quieras redirigir
+
+    return render(request, 'register.html')
